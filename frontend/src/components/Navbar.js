d91c21402b45dcf8876ae97a488a880eaccaa99d
theConsummateProfessional <ehood2@una.edu>
2020-09-22 13:07:41 -0500

    Added navbar

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Navbar() {
+    return (
+        <div>
+            <h2>NFL ELO</h2>
+            <div className="links">
+                <button>Simulations</button>
+                <button>Free Pick</button>
+                <button>Sign Up</button>
+                <button>Log In</button>
+            </div>
+        </div>
+    )
+}
