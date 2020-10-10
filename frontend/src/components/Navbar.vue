61829fff432f857a95364945210b8cfac3db39bd
theConsummateProfessional <ehood2@una.edu>
2020-10-10 11:25:25 -0500

    Added basic navbar component

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,5 +1,14 @@
 <template>
-    <div class="">
-        <h2>Navbar test bitch</h2>
-    </div>
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+        <b-navbar-brand href="/">NFL Betting Service</b-navbar-brand>
+
+        <!--Right Aligned Items-->
+        <b-navbar-nav class="ml-auto">
+            <b-nav-item href="#" right>Simulation Data</b-nav-item>
+            <b-nav-item href="#" right>Free Pick</b-nav-item>
+            <b-nav-item href="#" right>Login</b-nav-item>
+            <b-nav-item href="#" right>Sign Up</b-nav-item>
+        </b-navbar-nav>
+
+    </b-navbar>
 </template>
