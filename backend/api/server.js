e19c6c3c7cccb1fe684f4b7349c4667e4ad03141
theConsummateProfessional <ehood2@una.edu>
2020-10-11 00:36:10 -0500

    B001 added basic components to restAPI

---
+++ backend/api/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+mongoose.connect('mongodb://database/teams', { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', (error) => console.log(error));
+db.once('open', (error) => console.log('Connected to Database'));
+
+app.use(express.json());
+app.use(cors());
+
+const teamRouter = require('./routes/teams');
+app.use('/teams', teamRouter);
+
+app.listen(3000, () => console.log('Server Started'));
