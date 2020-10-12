49f813a334f8250fe4a3dc47dcfd26199c1b53c6
theConsummateProfessional <ehood2@una.edu>
2020-10-11 23:44:21 -0500

    B001 Added server outline, need to fix tomorrow

---
+++ backend/api/models/team.js
@@ -0,0 +1,128 @@
+const mongoose = require('mongoose');
+
+const teamSchema = new mongoose.Schema({
+    city: {
+        type: String,
+        required: true
+    },
+    mascot: {
+        type: String,
+        required: true
+    },
+    year: {
+        type: String,
+        required: true
+    },
+    yards_gained: {
+        type: Number,
+        required: true
+    },
+    yards_allowed: {
+        type: Number,
+        required: true
+    },
+    offense_yards_rank: {
+        type: Number,
+        required: true
+    },
+    defense_yards_rank: {
+        type: Number,
+        required: true
+    },
+    scoring_offense: {
+        type: Number,
+        required: true
+    },
+    scoring_defense: {
+        type: Number,
+        required: true
+    },
+    points_allowed: {
+        type: Number,
+        required: true
+    },
+    points_scored: {
+        type: Number,
+        required: true
+    },
+    num_turnovers_commited: {
+        type: Number,
+        required: true
+    },
+    turnover_committed_rank: {
+        type: Number,
+        required: true
+    },
+    turnovers_recieved: {
+        type: Number,
+        required: true
+    },
+    turnovers_recieved_rank: {
+        type: Number,
+        required: true
+    },
+    passing_offense_yards: {
+        type: Number,
+        required: true
+    },
+    passing_offense_rank: {
+        type: Number,
+        required: true
+    },
+    rushing_offense: {
+        type: Number,
+        required: true
+    },
+    rushing_offense_rank: {
+        type: Number,
+        required: true
+    },
+    passing_defense: {
+        type: Number,
+        required: true
+    },
+    passing_defense_rank: {
+        type: Number,
+        required: true
+    },
+    rushing_defense: {
+        type: Number,
+        required: true
+    },
+    rushing_defense_rank: {
+        type: Number,
+        required: true
+    },
+    average_time_of_possession: {
+        minutes: {
+            type: Number,
+            required: true
+        },
+        seconds: {
+            type: Number,
+            required: true
+        }
+    },
+    opponents_with_outcome: [
+            {
+                opponent_name: {
+                    type: String,
+                    required: true
+                },
+                team_score: {
+                    type: Number,
+                    required: true
+                },
+                opponent_score: {
+                    type: Number,
+                    required: true
+                },
+                week: {
+                    type: Number,
+                    required: true
+                }
+            }
+        ]
+});
+
+module.exports = mongoose.model('Team', teamSchema);
---
+++ backend/api/routes/teams.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const team = require('../models/team');
+const router = express.Router();
+const Team = require('../models/team');
+
+//Getting all
+router.get('/', async (req, res) => {
+    try {
+        const teams = await Team.find();
+        res.json(teams);
+    }
+    catch(err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+//Getting one
+router.get('/:id', getTeam, (req, res) => {
+    res.send(res.team);
+});
+
+//Creating one
+router.post('/', async(req, res) => {
+    const team = new Team({
+        city: req.body.city,
+        mascot: req.body.mascot,
+        year: req.body.year,
+        yards_gained: req.body.yards_gained,
+        yards_allowed: req.body.yards_allowed,
+        offense_yards_rank: req.body.offense_yards_rank,
+        defense_yards_rank: req.body.defense_yards_rank,
+        scoring_offense: req.body.scoring_offense,
+        scoring_defense: req.body.scoring_defense,
+        points_allowed: req.body.points_allowed,
+        points_scored: req.body.points_scored,
+        num_turnovers_commited: req.body.num_turnovers_commited,
+        turnover_committed_rank: req.body.turnover_committed_rank,
+        turnovers_recieved: req.body.turnovers_recieved,
+        turnovers_recieved_rank: req.body.turnovers_recieved_rank,
+        passing_offense_yards: req.body.passing_offense_yards,
+        passing_offense_rank: req.body.passing_offense_rank,
+        rushing_offense: req.body.rushing_offense,
+        rushing_offense_rank: req.body.rushing_offense_rank,
+        passing_defense: req.body.passing_defense,
+        passing_defense_rank: req.body.passing_defense_rank,
+        rushing_defense: req.body.rushing_defense,
+        rushing_defense_rank: req.body.rushing_defense_rank,
+        average_time_of_possession: {
+            minutes: req.body.average_time_of_possession.minutes,
+            seconds: req.body.average_time_of_possession.seconds
+        },
+        opponents_with_outcome: req.body.opponents_with_outcome
+    })
+})
+
+//update one
+
+//Deleting one
+router.delete('/:id', getTeam, async (req, res) => {
+    try {
+        await res.team.remove();
+        res.json({ message: 'Deleted Team' });
+    }
+    catch(err) {
+        return res.status(500).json({ message: err.message });
+    }
+})
+
+async function getTeam(req, res, next) {
+    let team;
+    try {
+        team = await Team.findById(req.params.id);
+        if(team == null) {
+            return res.status(400).json({ message: 'Cannot find Team' });
+        }
+    }
+    catch(err) {
+        return res.status(500).json({ message: err.message });
+    }
+
+    res.team = team;
+    next();
+}
+
+module.exports = router;
