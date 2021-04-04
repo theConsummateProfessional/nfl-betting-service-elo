b299e30bc172088dc6ab761acb08e60232bb96ae
theConsummateProfessional <ehood2@una.edu>
2021-04-04 10:41:09 -0500

    A0 got rid of some things

--- backend/api/routes/games.js
+++ backend/api/routes/games.js
@@ -15,7 +15,7 @@ router.get('/', async(req, res) => {
 
 //Getting one
 router.get('/:id', get_game_by_id, (req, res) => {
-    res.send(res.game);
+    res.send(res.game
 });
 
 //Getting one by year and week
